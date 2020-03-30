64cc2bed8cf8a1244f54fb4481f1bc358bdf0b24
miguel-enrique <miguelxzsmaster@gmail.com>
2020-03-30 20:35:48 -0300

    exelcicio do cebolinha completo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("digite algo para sair como o cebolinha diria: ");
+            string fala = Console.ReadLine();
+            Console.WriteLine(fala.Replace("rr","l").Replace("r","l").Replace("R","L"));
+            Console.ReadKey();
+        }
+    }
+}
